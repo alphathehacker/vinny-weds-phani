22ad36d58bc00a2b1c7c5e4e4068afc72471e783
alphathehacker <110968710+alphathehacker@users.noreply.github.com>
2026-08-10 20:31:46 +0530

    Add files via upload

---
+++ getsize.cjs
@@ -0,0 +1,4 @@
+const sizeOf = require('image-size');
+console.log('image1:', sizeOf('../image1.png'));
+console.log('image2:', sizeOf('../image2.png'));
+console.log('image3:', sizeOf('../image3.png'));
